7f7ed68ad999142b7e0c8ed3199ee06701b5ac73
wehuazi <ghwangd@163.com>
2017-09-21 10:55:25 +0800

    file manage

---
+++ WWKit/Main/Home/Controller/WWHomeViewController.h
@@ -0,0 +1,15 @@
+//
+//  WWHomeViewController.h
+//  WWKit
+//
+//  Created by joyman04 on 2017/9/20.
+//  Copyright © 2017年 wwkit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WWHomeViewController : UIViewController
+
+
+@end
+
---
+++ WWKit/Main/Home/Controller/WWHomeViewController.m
@@ -0,0 +1,30 @@
+//
+//  WWHomeViewController.m
+//  WWKit
+//
+//  Created by joyman04 on 2017/9/20.
+//  Copyright © 2017年 wwkit. All rights reserved.
+//
+
+#import "WWHomeViewController.h"
+
+@interface WWHomeViewController ()
+
+@end
+
+@implementation WWHomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor orangeColor];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
