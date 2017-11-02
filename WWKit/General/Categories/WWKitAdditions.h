682098efea7bd311e83b7ef96b768a3ff62a7de2
wehuazi <ghwangd@163.com>
2017-11-03 00:04:37 +0800

    add

--- WWKit/General/Categories/WWKitAdditions.h
+++ WWKit/General/Categories/WWKitAdditions.h
@@ -11,10 +11,7 @@
 #import "UIDevice+YYAdd.h"
 #import "UILabel+WWAdd.h"
 #import "UIImage+WWAdd.h"
-<<<<<<< HEAD
 #import "UIFont+WWTTF.h"
-=======
->>>>>>> a49abb7736b6575739b481ad91b2b14f73b26036
 #import "UIButton+Ext.h"
 #import "UIWindow+Ext.h"
 #import "UIColor+Ext.h"
@@ -24,11 +21,7 @@
 #import "NSDate+Utilities.h"
 #import "NSURL+Additions.h"
 #import "NSString+JSON.h"
-<<<<<<< HEAD
-
 #import "MBProgressHUD+WW.h"
-=======
 #import "UILabel+WWAdd.h"
 #import "MBProgressHUD+WW.h"
 
->>>>>>> a49abb7736b6575739b481ad91b2b14f73b26036
